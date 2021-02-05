e3bffeb3530d8a8b076820afcceb4e3fff925ada
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-02-05 10:48:22 -0800

    solve

--- challenges/review/4__js-fundamentals/4.js
+++ challenges/review/4__js-fundamentals/4.js
@@ -1,3 +1,10 @@
 // Create a function that calculates how many minutes have elapsed from midnight until right now.
 // The function should still return an accurate answer,
 // even if you ran it a few minutes or hours later.
+function minutesFromMidnight(){
+  let midnight = new Date();
+  const minutes = (midnight.getHours() * 60)+ midnight.getMinutes();
+  // console.log((midnight.getHours() * 60)+ midnight.getMinutes());
+  return "It has been " + minutes + " minutes from midnight."
+}
+console.log(minutesFromMidnight());
